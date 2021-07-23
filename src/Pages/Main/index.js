1f11595bb2301f271ef3b8258a00e07583032e44
Victor Oliveira <victor.souza.41@hotmail.com>
2021-07-22 21:22:06 -0300

    axios - hooks

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,23 +1,58 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {FaGithub, FaPlus} from 'react-icons/fa';
 
 import {Container, Form, SubmitButton} from './styles';
 
+import api from '../../services/api';
+
 export default function Main(){
+
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault();
+        async function submit(){
+
+            const response = await api.get(`repos/${newRepo}`);
+
+            const data = {
+                name: response.data.full_name,
+            }
+
+            setRepositorios([...repositorios, data]);
+            setNewRepo('');
+        }
+
+        submit();
+    }, [newRepo, repositorios]);
+
+    function handleinputChange(e){
+        setNewRepo(e.target.value);
+    }
+
     return(
         <Container>
-            <h1>
-                <FaGithub size={25}/>
-                Meus repositórios
-            </h1>
+      
+      <h1>
+        <FaGithub size={25}/>
+        Meus Repositorios
+      </h1>
 
-            <Form onsubmit={() => {}}>
-                <input type="text" placeholder="Adicionar repositórios"/>
-                <SubmitButton>
-                    <FaPlus color="#FFF" size={14} />
-                </SubmitButton>
-            </Form>
+      <Form onSubmit={handleSubmit}>
+        <input 
+        type="text" 
+        placeholder="Adicionar repositorios"
+        value={newRepo}
+        onChange={handleinputChange}
+        />
 
-        </Container>
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14}/>
+        </SubmitButton>
+
+      </Form>
+
+    </Container>
     );
 }
--- src/Pages/Main/styles.js
+++ src/Pages/Main/styles.js
@@ -34,7 +34,9 @@ export const Form = styled.form`
     }
 `;
 
-export const SubmitButton = styled.button`
+export const SubmitButton = styled.button.attrs({
+    type: 'submit'
+})`
     background: #0D2636;
     border: 0;
     border-radius: 4px;
