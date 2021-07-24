0da55de9b5f49b19317689b83d50367908948a3c
Victor Oliveira <victor.souza.41@hotmail.com>
2021-07-24 12:24:51 -0300

    Armazenado sessão de inclusão no localStorage. Criado verificação de itens já existente numa lista e informando alerta ao usuário

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa';
 
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
@@ -10,14 +10,41 @@ export default function Main(){
     const [newRepo, setNewRepo] = useState('');
     const [repositorios, setRepositorios] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [alert, setAlert] = useState(null);
+
+    //Buscar
+    useEffect(() => {
+      const repoStorage = localStorage.getItem('repos');
+
+      if(repoStorage){
+        setRepositorios(JSON.parse(repoStorage));
+      }
+    }, []);
+
+    //Salvar alterações
+    useEffect(() => {
+      localStorage.setItem('repos', JSON.stringify(repositorios))
+    }, [repositorios]);
 
     const handleSubmit = useCallback((e) => {
         e.preventDefault();
 
         async function submit(){
           setLoading(true);
+          setAlert(null);
           try{
+
+            if(newRepo === ''){
+              throw new Error('Você precisa informar um repositório.');
+            }
+
             const response = await api.get(`repos/${newRepo}`);
+
+            const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+            if(hasRepo){
+              throw new Error('Repositório já está na sua fila!');
+            }
 
             const data = {
                 name: response.data.full_name,
@@ -26,7 +53,8 @@ export default function Main(){
             setRepositorios([...repositorios, data]);
             setNewRepo('');
           }catch(error){
-            console.log('Error');
+            setAlert(true)
+            console.log(error);
           }
           finally{
             setLoading(false);
@@ -38,6 +66,7 @@ export default function Main(){
 
     function handleinputChange(e){
         setNewRepo(e.target.value);
+        setAlert(null);
     }
 
     const handleDelete = useCallback((repo) => {
@@ -53,7 +82,7 @@ export default function Main(){
         Meus repositorios
       </h1>
 
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} error={alert}>
         <input 
         type="text" 
         placeholder="Adicionar repositorios"
