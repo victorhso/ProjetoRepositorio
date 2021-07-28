eeac528fbe07f114ed66e63be7e41842d94bf80b
Victor Oliveira <victor.souza.41@hotmail.com>
2021-07-27 21:15:40 -0300

    Carregando detalhes

--- src/Pages/Repositorio/index.js
+++ src/Pages/Repositorio/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Container} from './style';  
+import api from '../../services/api';
+
+//decodeURIComponent(match.params.repositorio)
 
 export default function Repositorio({match}){
+
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function load(){
+            const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state:'open',
+                        per_page: 5
+                    }
+                })
+            ]);
+
+            setRepositorio(repositorioData.data);
+            setIssues(issuesData.data);
+            setLoading(false);
+        }
+
+        load();
+    }, [match.params.repositorio])
+
     return(
-        <h1 style={{color:'#FFF'}}>
-            {decodeURIComponent(match.params.repositorio)}
-        </h1>
+        <Container style={{color:'#FFF'}}>
+            {}
+        </Container>
     );
 }
