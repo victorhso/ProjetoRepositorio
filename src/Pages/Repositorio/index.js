95ee44f956419d6a42d3af012027b12c37fc9825
Victor Oliveira <victor.souza.41@hotmail.com>
2021-08-03 21:16:36 -0300

    exibindo issues

--- src/Pages/Repositorio/index.js
+++ src/Pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {FaArrowLeft} from 'react-icons/fa';
-import {Container, Owner, Loading, BackButton} from './style';  
+import {Container, Owner, Loading, BackButton, IssuesList} from './style';  
 import api from '../../services/api';
 
 //decodeURIComponent(match.params.repositorio)
@@ -51,6 +51,25 @@ export default function Repositorio({match}){
                 <h1>repositorio.name</h1>
                 <p>{repositorio.description}</p>
             </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt ={issue.user.login}/>
+                        
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}>{issue.title}</a>
+                                {issue.labels.map(label => (
+                                    <span key={String(label.id)}>{label.name}</span>
+                                ))}
+                            </strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+
+                    </li>
+                ))}
+            </IssuesList>
         </Container>
     );
 }
